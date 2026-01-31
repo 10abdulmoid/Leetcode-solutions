cc4b6de7f236208df0c8d0fab09ff5c7d2ea75cf
Mohammad Abdul Moid <abdulmoid@Mohammads-MacBook-Air.local>
2026-01-31 21:31:13 +0530

    ill add it

---
+++ two_sum.py
@@ -0,0 +1,9 @@
+class solutions:
+    def twosum(self,nums, target):
+        prevMap = {}
+        for i,n in enumerate[nums]:
+            diff = target - n
+            if diff in prevMap:
+                return (prevMap(diff),i)
+            prevMap[n]= i
+        return        
