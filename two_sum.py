54105cecad64ca30d62b6076a215e9245acf6c78
Mohammad Abdul Moid <abdulmoid@Mohammads-MacBook-Air.local>
2026-01-31 22:03:00 +0530

    fixed

--- two_sum.py
+++ two_sum.py
@@ -1,9 +1,16 @@
+<<<<<<< HEAD
 class Solutions:
     def twosum(self,nums, target):
         prevMap = {}
         for i,n in enumerate[nums]:
+=======
+class Solution:
+    def twosum(self, nums, target):
+        Hash = {}
+        for i, n in enumerate [nums]:
+>>>>>>> 8fa8685 (fixed)
             diff = target - n
-            if diff in prevMap:
-                return (prevMap(diff),i)
-            prevMap[n]= i
-        return        
+            if diff in Hash:
+                return(Hash[diff], i)
+            Hash[n] = i
+        return    
